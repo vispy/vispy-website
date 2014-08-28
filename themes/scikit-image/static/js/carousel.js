5e37ba0599ae1b2c913daf6794e0abb7718d335e
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-08-28 12:22:55 +0100

    WIP: moving a few static files around.

---
+++ themes/scikit-image/static/js/carousel.js
@@ -0,0 +1,43 @@
+  function shuffle(array) {
+     var elementsRemaining = array.length, temp, randomIndex;
+     while (elementsRemaining > 1) {
+        randomIndex = Math.floor(Math.random() * elementsRemaining--);
+        if (randomIndex != elementsRemaining) {
+           temp = array[elementsRemaining];
+           array[elementsRemaining] = array[randomIndex];
+           array[randomIndex] = temp;
+        }
+     }
+     return array;
+  }
+  
+  $('#carousel ul').anoSlide(
+     {
+        items: 2,
+        lazy: true,
+        speed: 1000,
+        auto: 4000,
+        delay: 100,
+        autostop: false,
+        
+        onConstruct: function(instance)
+        {
+           shuffle(instance.slides);
+        },
+        /* rewind should be instant */
+        onStart: function(ui)
+        {
+           if (ui.index==0) 
+           {
+              ui.instance.options.speed = 0;
+           }            
+        },
+        onEnd: function(ui)
+        {
+           if (ui.index==0) 
+           {
+              ui.instance.options.speed = 1000;
+           }            
+        },
+     })
+     
